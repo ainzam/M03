281d91541530b437a5e18eb29f5b289d6b5a7a80
cfgs <cfgs@pc-info-host>
2023-09-26 20:02:56 +0200

    blud

---
+++ UF4/src/exercici14/exercici14.java
@@ -0,0 +1,26 @@
+package exercici14;
+
+public class exercici14 {
+	public static void main(String[] args) {
+		int[] vectorEnters = { 1, 2, 3, 4, 5 }; // Exemple de vector d'enters
+
+		// Crèiem un vector de Strings a partir del vector d'enters
+		String[] vectorStrings = convertirVectorEntersAStrings(vectorEnters);
+
+		// Mostrem el vector de Strings
+		for (String s : vectorStrings) {
+			System.out.println(s);
+		}
+	}
+
+	public static String[] convertirVectorEntersAStrings(int[] vectorEnters) {
+		String[] vectorStrings = new String[vectorEnters.length];
+
+		for (int i = 0; i < vectorEnters.length; i++) {
+			// Utilitzem String.valueOf() per convertir l'enter a String
+			vectorStrings[i] = String.valueOf(vectorEnters[i]);
+		}
+
+		return vectorStrings;
+	}
+}
