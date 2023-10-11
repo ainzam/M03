98745ef81a8d1a2638bcf0c1f1116440babb0f7f
cfgs <cfgs@pc-info-host>
2023-10-11 20:58:06 +0200

    blud

--- UF4/src/exercici24/Exercici24.java
+++ UF4/src/exercici24/Exercici24.java
@@ -1,7 +1,9 @@
 package exercici24;
 
-public class Exercici24 {
-	public static void main(String[] args) {
+import m3.utils.Modul;
 
+public class Exercici24 {
+	public static void main(String[] args) throws Exception {
+		System.out.println("El mòdul 1 és " + new Modul().getModulCurt(1));
 	}
 }
