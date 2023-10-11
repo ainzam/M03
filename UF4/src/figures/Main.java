cf44a4afb9d60360374a408b8d3c1cab9afcfde2
cfgs <cfgs@pc-info-host>
2023-10-11 19:34:31 +0200

    blud

--- UF4/src/figures/Main.java
+++ UF4/src/figures/Main.java
@@ -1,7 +1,28 @@
 package figures;
 
 public class Main {
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		// Crear un array de Figura para almacenar círculos y cuadrados
+		Figura[] figuras = new Figura[4];
+
+		// Crear e inicializar círculos y cuadrados
+		figuras[0] = new Cercle();
+		((Cercle) figuras[0]).setRadi(5);
+
+		figuras[1] = new Cercle();
+		((Cercle) figuras[1]).setRadi(3);
+
+		figuras[2] = new Quadrat();
+		((Quadrat) figuras[2]).setCostat(4);
+
+		figuras[3] = new Quadrat();
+		((Quadrat) figuras[3]).setCostat(6);
+
+		// Calcular e imprimir los perímetros
+		for (Figura figura : figuras) {
+			double perimetre = figura.calcularPerimetre();
+			System.out.println("Perímetro: " + perimetre);
+		}
 	}
 }
